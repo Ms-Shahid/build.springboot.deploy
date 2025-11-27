42b47daeb8dcd1bedba127dbf3e4639a814fcdfa
Ms-Shahid <ms2406shahid@gmail.com>
2025-11-27 21:23:54 +0530

    Intro to spring & springboot

---
+++ week-1/codingstuttle/src/main/java/app/coding/codingstuttle/CodingstuttleApplication.java
@@ -0,0 +1,13 @@
+package app.coding.codingstuttle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingstuttleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingstuttleApplication.class, args);
+	}
+
+}
---
+++ week-1/codingstuttle/src/test/java/app/coding/codingstuttle/CodingstuttleApplicationTests.java
@@ -0,0 +1,13 @@
+package app.coding.codingstuttle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingstuttleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
